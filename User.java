ecea514c7ee0cfe7e25f989de87d70ce717d3d04
gaetan <gaetan.roche@etu.univ-lyon1.fr>
2016-03-12 16:17:04 +0100

    modifié:         User.java 	modifié:         UserList.java

--- User.java
+++ User.java
@@ -1,16 +1,25 @@
-package ServerPop3;
 
 import java.util.ArrayList;
 
 public class User {
-	private String name;
-	private String password;
-	private ArrayList<Message> mails;
-	
-	public User(String name, String password){
-		this.name = name;
-		this.password = password;
-		//TO DO handle mails arraylist
-	}
+    private int id;
+    private String name;
+    private String password;
+    private ArrayList<Message> mails;
+
+    public User(int id, String name, String password){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        //TO DO handle mails arraylist
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    public boolean connect(String password) {
+        return this.password.compareTo(password) == 0;
+    }
 	
 }
--- UserList.java
+++ UserList.java
@@ -1,21 +1,55 @@
-package ServerPop3;
 
+import java.io.BufferedReader;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserList {
 	ArrayList<User> userList;
 	
 	public UserList(){
-		//TO DO open file and load all users.
-	}
+            //TO DO open file and load all users.
+            FileInputStream fis;
+            try{
+                fis = new FileInputStream(new File("users.txt"));
+                InputStreamReader lecteur = new InputStreamReader(fis);
+                BufferedReader buff = new BufferedReader(lecteur);
+                String ligne;
+                while((ligne=buff.readLine())!=null){
+                    String[] userIds = ligne.split(" ");
+                    userList.add(new User(Integer.parseInt(userIds[0]), userIds[1], userIds[2]));
+                }
+            }catch(FileNotFoundException e){
+                System.out.println(e.getMessage());
+            } catch (IOException ex) {
+                Logger.getLogger(UserList.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 	
 	public User connect(String name, String password){
-		return new User("test", "test");
+            for(User mailUser : this.userList){
+                if(mailUser.getName().compareTo(name) == 0){
+                    if(mailUser.connect(password))
+                        return mailUser;
+                    else
+                        return null;
+                }
+            }
+            return null;
 	}
 
 	public String chechUser(String userName) {
-		// TODO Auto-generated method stub
-		return null;
+            for(User mailUser : this.userList){
+                if(mailUser.getName().compareTo(userName) == 0)
+                    return userName;
+            }
+            return null;
 	}
 	
 }
